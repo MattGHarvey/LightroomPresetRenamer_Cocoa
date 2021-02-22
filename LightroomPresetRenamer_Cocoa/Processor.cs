f23a9d1867d989d1d0cfc30e2b33c518a8adb036
Matt Harvey <matt@75central.com>
2021-02-22 13:53:19 -0600

    Playing with icons

---
+++ LightroomPresetRenamer_Cocoa/Processor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using XmpCore;
+using System.Globalization;
+namespace LightroomPresetRenamer_Cocoa
+{
+    public class Processor
+    {
+        public static void TraverseDirectory(DirectoryInfo directoryInfo)
+        {
+            var subdirectories = directoryInfo.EnumerateDirectories();
+
+            foreach (var subdirectory in subdirectories)
+            {
+                TraverseDirectory(subdirectory);
+            }
+
+            var files = directoryInfo.EnumerateFiles();
+
+            foreach (var file in files)
+            {
+                // 
+                if (file.Extension == ".xmp")
+                {
+                    //   Console.WriteLine(file);
+                    // if (file.Name.Contains("Velvia"))
+                    //    {
+             //       HandleFile(file);
+                    //   }
+                }
+            }
+        }
+    }
+}
--- LightroomPresetRenamer_Cocoa/ViewController.cs
+++ LightroomPresetRenamer_Cocoa/ViewController.cs
@@ -21,8 +21,23 @@ namespace LightroomPresetRenamer_Cocoa
         {
             //throw new NotImplementedException();
             mainLabel.StringValue = "Clicked";
+            var dlg = NSOpenPanel.OpenPanel;
+            dlg.CanChooseFiles = false;
+            dlg.CanChooseDirectories = true;
+            //   dlg.AllowedFileTypes = new string[] { "txt", "html", "md", "css" };
+            // dlg.RunModal();
+            //    List<Result> fileSearchResults = new List<Result>();
+            if (dlg.RunModal() == 1)
+            {
+                // Nab the first file
+                var url = dlg.Urls[0];
+                if (url != null)
+                {
+                    var path = url.Path;
+               //     SearchFileNames(path);
+                }
+            }
         }
-
         public override NSObject RepresentedObject
         {
             get
